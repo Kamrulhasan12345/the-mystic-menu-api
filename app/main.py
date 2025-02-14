5eec3801af10e0c7815a334fe425870842c4cd4c
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2025-02-14 04:46:58 +0000

    [initial]: first boilerplate

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routers import auth, recipes
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, prefix="/auth")
+app.include_router(recipes.router, prefix="/recipes")
